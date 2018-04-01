cc79afd62f0f97b4b1bf853d77eb81769e3bb39c
Rajesh Kumar <rajesh.kumar03@nagarro.com>
2018-04-01 22:05:03 +0530

    Adding files to master

---
+++ WebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpPost]
+        public void Save(Employee employee)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View("Save", new Employee());
+        }
+    }
+}
---
+++ WebApp/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WebApp
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public string FavouriteMovie { get; set; }
+        public List<SelectListItem> Movies
+        {
+            get
+            {
+                List<SelectListItem> items = new List<SelectListItem>();
+
+                items.Add(new SelectListItem { Text = "Action", Value = "0" });
+
+                items.Add(new SelectListItem { Text = "Drama", Value = "1" });
+
+                items.Add(new SelectListItem { Text = "Comedy", Value = "2", Selected = true });
+
+                items.Add(new SelectListItem { Text = "Science Fiction", Value = "3" });
+                return items;
+            }
+            private set { }
+        }
+
+    }
+}
